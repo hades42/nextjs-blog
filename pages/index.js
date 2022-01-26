300e6539e11a977b78fb7a8ae7313db4281e7f2f
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-01-26 21:14:05 +1100

    Fetching each page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import utilStyles from "../components/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
+import { Container } from "@chakra-ui/react";
 
 export async function getStaticProps() {
     const allPostsData = getSortedPostsData();
@@ -16,37 +17,41 @@ export async function getStaticProps() {
 
 export default function Home({ allPostsData }) {
     return (
-        <Layout home>
-            <Head>
-                <title>{siteTitle}</title>
-            </Head>
-            <section className={utilStyles.headingMd}>
-                <p>[Your Self Introduction]</p>
-                <p>
-                    (This is a sample website - you’ll be building a site like
-                    this on{" "}
-                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>
-                    .)
-                </p>
-            </section>
-            <section
-                className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
-            >
-                <h2 className={utilStyles.headingLg}>Blog</h2>
-                <ul className={utilStyles.list}>
-                    {allPostsData.map(({ id, date, title }) => (
-                        <li className={utilStyles.listItem} key={id}>
-                            <Link href={`/posts/${id}`}>
-                                <a>{title}</a>
-                            </Link>
-                            <br />
-                            <small className={utilStyles.lightText}>
-                                <Date dateString={date} />
-                            </small>
-                        </li>
-                    ))}
-                </ul>
-            </section>
-        </Layout>
+        <Container maxWidth="container.xl" padding={0}>
+            <Layout home>
+                <Head>
+                    <title>{siteTitle}</title>
+                </Head>
+                <section className={utilStyles.headingMd}>
+                    <p>[Your Self Introduction]</p>
+                    <p>
+                        (This is a sample website - you’ll be building a site
+                        like this on{" "}
+                        <a href="https://nextjs.org/learn">
+                            our Next.js tutorial
+                        </a>
+                        .)
+                    </p>
+                </section>
+                <section
+                    className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
+                >
+                    <h2 className={utilStyles.headingLg}>Blog</h2>
+                    <ul className={utilStyles.list}>
+                        {allPostsData.map(({ id, date, title }) => (
+                            <li className={utilStyles.listItem} key={id}>
+                                <Link href={`/posts/${id}`}>
+                                    <a>{title}</a>
+                                </Link>
+                                <br />
+                                <small className={utilStyles.lightText}>
+                                    <Date dateString={date} />
+                                </small>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            </Layout>
+        </Container>
     );
 }
