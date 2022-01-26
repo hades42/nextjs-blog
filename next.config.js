811ecf0127d7d977d9658e46aaf6460fdf474b71
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-01-26 21:14:29 +1100

    Trying MDX

---
+++ next.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    // Prefer loading of ES Modules over CommonJS
+    experimental: { esmExternals: true },
+    // Support MDX files as pages:
+    pageExtensions: ["md", "mdx", "tsx", "ts", "jsx", "js"],
+    // Support loading `.md`, `.mdx`:
+    webpack(config, options) {
+        config.module.rules.push({
+            test: /\.mdx?$/,
+            use: [
+                // The default `babel-loader` used by Next:
+                options.defaultLoaders.babel,
+                {
+                    loader: "@mdx-js/loader",
+                    /** @type {import('@mdx-js/loader').Options} */
+                    options: {
+                        /* jsxImportSource: …, otherOptions… */
+                    },
+                },
+            ],
+        });
+
+        return config;
+    },
+};
